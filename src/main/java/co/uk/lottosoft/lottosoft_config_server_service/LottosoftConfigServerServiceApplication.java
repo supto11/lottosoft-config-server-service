a3e776276753b0954a51080dc81865cb2a1e731e
Supto Purakayasto <codersupto@gmail.com>
2022-07-27 14:01:04 +0600

    Initial commit by Supto Purakayasto

---
+++ src/main/java/co/uk/lottosoft/lottosoft_config_server_service/LottosoftConfigServerServiceApplication.java
@@ -0,0 +1,13 @@
+package co.uk.lottosoft.lottosoft_config_server_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottosoftConfigServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottosoftConfigServerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/uk/lottosoft/lottosoft_config_server_service/LottosoftConfigServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.lottosoft.lottosoft_config_server_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottosoftConfigServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
